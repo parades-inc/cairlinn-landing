7bd491cdbaa41de81686d6df70f60f7ced31307f
saagarvaru <saagarv14@gmail.com>
2022-01-16 18:58:16 -0800

    fixed more stuff

--- src/components/Home/JoinUs/JoinUs.tsx
+++ src/components/Home/JoinUs/JoinUs.tsx
@@ -1,27 +1,42 @@
 import { FC } from 'react';
 import './joinUs.css';
 import joinUsImage from '../../../assets/images/joinUsImage.png';
-import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import {Box, Button, FormControl, Grid, Stack, Typography} from '@mui/material';
 import { TextField } from '@mui/material';
 
 export const JoinUs: FC = () => {
   return (
     <div id="joinUs">
       <Box className="join-us-container">
-        <img src={joinUsImage} />
-        <Grid container justifyContent={'right'}>
-          <Grid item xs={12}>
-            <Typography className="join-us-text" variant="h1">Join Us</Typography>
-            <TextField label="Email" placeholder="Email" />
-            <TextField
-              label="DOB (MM/DD/YYYY)"
-              placeholder="DOB (MM/DD/YYYY)"
-            />
-            <TextField
-              label="ZIP CODE"
-              placeholder="ZIP CODE"
-            />
-            <Button color="primary" variant={'contained'}>GET NOTIFIED</Button>
+        <Grid container justifyContent={'center'}>
+          <Grid item xs={12} md={4}>
+
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box className={'join-us-content'}>
+              <Typography className="join-us-text" variant="h1">Join Us</Typography>
+              <form>
+                <FormControl fullWidth>
+                  <TextField
+                    label="Email"
+                    placeholder="Email"
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <TextField
+                    label="DOB (MM/DD/YYYY)"
+                    placeholder="DOB (MM/DD/YYYY)"
+                  />
+                </FormControl>
+                <FormControl fullWidth>
+                  <TextField
+                    label="ZIP CODE"
+                    placeholder="ZIP CODE"
+                  />
+                </FormControl>
+                <Button color="primary" variant={'contained'}>GET NOTIFIED</Button>
+              </form>
+            </Box>
           </Grid>
         </Grid>
       </Box>
--- src/config/muiTheme.ts
+++ src/config/muiTheme.ts
@@ -24,7 +24,6 @@ export const theme = createTheme({
           color: '#FAF7ED',
           fontSize: 24,
           padding: '16px 20px 8px 20px',
-
           fontStyle: 'normal',
           fontWeight: 'bold',
           fontFamily: ['\'DIN Condensed\''].join(','),
@@ -34,17 +33,12 @@ export const theme = createTheme({
     MuiTextField: {
       styleOverrides: {
         root: {
-          label: 'outlined',
-          variant: 'outlined',
-          // input text needs to be centered
-          // input text needs to be white
-          // input text needs to not be impacted by opaqueness
           backgroundColor: 'grey', // needs to be black but then text doesnt appear
-          opacity: 0.4,
+          '::placeholder' : {
+            color: '#327371'
+          },
+          marginBottom: 10
         },
-
-        // input: {}, // this causes errors?
-        // FilledInput: {} // same here
       },
     },
   },
